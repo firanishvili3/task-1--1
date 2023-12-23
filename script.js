1e475e94640477628b6edbc188f06dc9ec1a5da5
anafiranishvili1 <ana.firanisgvili@mail.ru>
2023-12-23 11:26:27 +0400

    first commit
    first task 1

---
+++ script.js
@@ -0,0 +1,44 @@
+ 
+// 1
+
+let number1 = 100;
+
+
+if(number1 < 50){
+    console.log("<50")
+}else if( number1 > 20){
+    console.log( "მეტია 20-ზე")
+}else { console.log("false")}
+
+
+// 2
+
+let userName = "mariami"
+  
+// 33
+
+
+
+if(userName){ console.log("true")}else{console.log("false")}
+
+// 4
+
+let userName2 = "mariami"
+
+// 5
+switch (userName2){ case "mariami" :
+console.log("true")
+break;
+case ("else") :
+}
+
+// 6
+
+let info = prompt ("enter a number")
+console.log("info")
+
+
+let number = 10;
+if (number %2 == 0){ console.log("kenti")}
+
+if (number %2 == 1){console.log("luwi")}
